7274ba016d72b31bf0caa1ecd02a4ab77035f673
janreiroblox09 <jlayderos2025@gmail.com>
2025-04-03 13:45:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class AveragesData(BaseModel):
     soilPH: float
 
 # ✅ Raspberry Pi Flask Server URL
-RPI_URL = "http://192.168.1.101:5000/receive-data"
+RPI_URL = "http://192.168.1.10:5000/receive-data"
 
 # ✅ Global variable to store the latest data and recommendation
 latest_data = None
